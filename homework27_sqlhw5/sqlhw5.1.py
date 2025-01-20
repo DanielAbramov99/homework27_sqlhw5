b88f48b1e9110ab0ad3d90d5c4d53635d46b1191
DanielAbramov99 <daniilabramov99@gmail.com>
2025-01-20 02:12:33 +0200

    Add files via upload

---
+++ homework27_sqlhw5/sqlhw5.1.py
@@ -0,0 +1,13 @@
+# 1
+# "Partition" is used to split the data into subsets in this instance is used to split each employee to calculate total sales for each  one.
+
+# 2
+# -> OR ->> used in sql to interact with json data while -> used to return json object or array element and ->> used to return text value of json object or array element.
+
+# 3
+# This code is used to create a table that stores numbers arrays and after that it use the select option to check first element of each array and print it in another column called first element,
+# the second select used to show only rows that have at least one number 3 in the array.
+
+# 4
+# "Generated always" is used to create a column that always generated and calculated based on other column or columns on this table,
+# in this instance the "generated always" is used to make a column named "tax" that calculated by using the column "price" and multiplying each value of that column by 0.2.
